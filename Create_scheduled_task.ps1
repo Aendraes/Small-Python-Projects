ec55315c47cbfe5753f5d29dec2da59c22590381
Aendraes <aendraes931@gmail.com>
2022-11-24 19:34:48 +0100

    Delete Create_scheduled_task.ps1

--- Create_scheduled_task.ps1
+++
@@ -1,8 +0,0 @@
-$Action = New-ScheduledTaskAction -Execute 'python.exe' -Argument 'multiclipboard.py'
-$Trigger = New-ScheduledTaskTrigger -AtLogOn
-$Settings = New-ScheduledTaskSettingsSet 
-$Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Settings $Settings
-$Username = $(whoami)
-$Password = Read-Host "Please enter your password." -AsSecureString
-$TaskPrincipal = New-ScheduledTaskPrincipal -userId $Username -LogonType S4U
-Register-ScheduledTask -TaskName 'Multiclipboard' -InputObject $Task -User $Username -Password $Password
